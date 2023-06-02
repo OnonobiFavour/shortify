70acab1f7a6c008874ac2473a9466d4d4f091e8f
unknown <phayvorononobi@gmail.com>
2023-06-01 19:14:19 -1100

    my commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './components/Home'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Signup from './components/Signup'
 import Login from './components/Login'
 
