5ecc54c4c20965b417b3515c8f6c21842f0e6d26
unknown <phayvorononobi@gmail.com>
2023-06-08 06:04:26 -1100

    update: Added icons

--- src/components/Dash.tsx
+++ src/components/Dash.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './Dash.css'
+import {AiOutlineQrcode} from 'react-icons/ai'
+import {HiOutlineScissors} from 'react-icons/hi'
+import {MdOutlineDomainAdd} from 'react-icons/md'
+import {BiCustomize} from 'react-icons/bi'
+import {GoMail} from 'react-icons/go'
+import {RxExit} from 'react-icons/rx'
 
 const Dash = () => {
   return (
@@ -7,10 +13,11 @@ const Dash = () => {
         <div className="side_bar">
           <h1>Dashboard</h1>
             <ul>
-                <ol><a href="#">Trim Url</a></ol>
-                <ol><a href="#">Create QR code</a></ol>
-                <ol><a href="#">Custom Url</a></ol>
-                <ol><a href="#">Domain Integration</a></ol>
+                <ol><a href="#"><HiOutlineScissors className='icons' size={20}></HiOutlineScissors>Trim Url</a></ol>
+                <ol><a href="#"><AiOutlineQrcode className='icons' size={20}></AiOutlineQrcode>Create QR code</a></ol>
+                <ol><a href="#"><BiCustomize className='icons' size={20}></BiCustomize>Custom Url</a></ol>
+                <ol><a href="#"><MdOutlineDomainAdd className = 'icons' size={20}></MdOutlineDomainAdd>Domain Integration</a></ol>
+                <ol><a href="#"><RxExit className = 'icons' size={20}></RxExit>Exit</a></ol>
             </ul>
         </div>
         <main>
@@ -46,7 +53,7 @@ const Dash = () => {
                             <h3>0</h3>
                         </div>
                     </div>
-                    <button className="email">Send as Mail</button>
+                    <button className="email">Send as Mail <GoMail size={20}></GoMail></button>
                 </div>
             </div>
         </main>
